7085667af35c9d84e2028f7ffb05fcbe8def1b3d
Gaming friends <uttamkolaligowda@gmail.com>
2020-12-11 16:22:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+print(type(height))
+#Convert the str type to int type with a variable
+int_weight=int(weight)
+#Height should be float
+int_height=float(height)
+#Calculate the bmi using type
+
+bmi=int_weight/(int_height**2)
+print(type(bmi))
+#Convert the float type to int type
+
+int_bmi=int(bmi)
+print(type(int_bmi))
+print(int_bmi)
+
+
+
+
+
+
+
+
+
+
+
